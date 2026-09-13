ce4418308a13dc5a2728224d325773b25f7bf8cd
EasyLab Dev <dev@easylab.local>
2026-09-13 18:31:22 +0000

    publish: maven-publish to GitHub Packages as io.github.easy-utils:easy-rpc-kotlin:0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    `maven-publish`
 }
 
 group = "easyrpc"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories { mavenCentral() }
 
@@ -18,3 +19,24 @@ dependencies {
 }
 
 kotlin { jvmToolchain(17) }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "io.github.easy-utils"
+            artifactId = "easy-rpc-kotlin"
+            version = "0.2.0"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/easy-utils/easy-rpc-kotlin")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: ""
+                password = System.getenv("GITHUB_TOKEN") ?: ""
+            }
+        }
+    }
+}
